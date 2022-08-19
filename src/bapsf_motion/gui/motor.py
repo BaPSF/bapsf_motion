366874df8b44d27d63c4767083305363c6def8b1
Erik <eteverson@gmail.com>
2022-08-19 16:30:29 -0700

    initial layout for the controls

--- src/bapsf_motion/gui/motor.py
+++ src/bapsf_motion/gui/motor.py
@@ -62,6 +62,10 @@ class MotorGUI(QMainWindow):
         "verbosity_label": None,
         "log_box": None,
     }  # type: Dict[str, Any]
+    _control_widgets = {
+        "ip": None,
+        "stop": None,
+    }  # type: Dict[str, Any]
 
     def __init__(self):
         super().__init__()
@@ -72,9 +76,12 @@ class MotorGUI(QMainWindow):
 
         layout = QHBoxLayout()
 
-        control_widget = QLabel()
-        control_widget.set_frame_style(QFrame.StyledPanel | QFrame.Plain)
+        # control_widget = QLabel()
+        # control_widget.set_frame_style(QFrame.StyledPanel | QFrame.Plain)
+        # control_widget.set_minimum_width(800)
+        control_widget = QWidget()
         control_widget.set_minimum_width(800)
+        control_widget.set_layout(self.control_layout)
         layout.add_widget(control_widget, stretch=1100)
 
         vbar = QLabel()
@@ -148,6 +155,64 @@ class MotorGUI(QMainWindow):
         return layout
 
     @property
+    def control_layout(self):
+        layout = QVBoxLayout()
+
+        # row 1: Title
+        label = QLabel("Motor Class Debugger GUI")
+        font = label.font()
+        font.set_point_size(16)
+        font.set_bold(True)
+        label.set_font(font)
+        layout.add_widget(label, alignment=Qt.AlignHCenter | Qt.AlignTop)
+
+        # row 2: STOP Button
+        # stop_btn = QPushButton("STOP")
+        stop_btn = StopButton("STOP")
+        stop_btn.set_fixed_height(72)
+        font = stop_btn.font()
+        font.set_point_size(36)
+        font.set_bold(True)
+        stop_btn.set_font(font)
+        # stop_btn.set_style_sheet("""
+        # background-color: rgb(255,90,90);
+        # border-radius: 6px;
+        # border: 2px solid black;
+        # box-shadow: -3px 3px orange, -2px 2px orange, -1px 1px orange;
+        # }
+        # """)
+        # stop_btn.clicked.connect(self.stop_moving)
+        self._control_widgets["stop"] = stop_btn
+        layout.add_widget(stop_btn)
+
+        # row 3: Initial Controls/Indicators
+        row2_layout = QHBoxLayout()
+
+        label = QLabel("IP Address:")
+        font = label.font()
+        font.set_point_size(12)
+        label.set_font(font)
+        row2_layout.add_widget(label, alignment=Qt.AlignTop)
+
+
+        ip_widget = QLineEdit()
+        ip_widget.set_input_mask("000.000.000.000")
+        ip_widget.set_maximum_width(100)
+        ip_widget.set_size_policy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        row2_layout.add_spacing(4)
+        row2_layout.add_widget(ip_widget, alignment=Qt.AlignLeft)
+
+        row2_layout.add_stretch()
+
+        layout.add_layout(row2_layout)
+
+        # full bottom with blank space
+        layout.add_stretch()
+        # layout.add_spacing(self.height())
+
+        return layout
+
+    @property
     def log_verbosity(self):
         return self._log_verbosity
 
@@ -158,6 +223,12 @@ class MotorGUI(QMainWindow):
     def update_log_verbosity(self, value):
         self.log_verbosity = value
         self._log_widgets["verbosity_label"].set_text(f"{value}")
+
+    def stop_moving(self):
+        # send command to stop moving
+        ...
+
+
 
 
 if __name__ == "__main__":
