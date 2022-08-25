3c8f7c8e66004c7fc898be2a231ef00eb8c6b665
Erik <eteverson@gmail.com>
2022-08-24 20:32:09 -0700

    add a QValidator for validating ipv4 addresses

--- src/bapsf_motion/gui/widgets.py
+++ src/bapsf_motion/gui/widgets.py
@@ -1,10 +1,33 @@
 import logging
 import math
 
+from PySide6.QtGui import QValidator
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QPushButton, QTextEdit, QPlainTextEdit
 
 from __feature__ import snake_case  # noqa
+
+from bapsf_motion.controllers.motor import _ipv4_pattern
+
+
+class IPv4Validator(QValidator):
+    def __init__(self, logger=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._pattern = _ipv4_pattern
+
+        log_name = "" if logger is None else f"{logger.name}."
+        log_name += "IPv4Validator"
+        self._logger = logging.getLogger(log_name)
+
+    def validate(self, arg__1: str, arg__2: int) -> object:
+        string = arg__1.replace("_", "")
+
+        match = self._pattern.fullmatch(string)
+        if match is None:
+            self._logger.warning(f"IP address is invalid, '{string}'.")
+            return QValidator.Intermediate
+
+        return QValidator.Acceptable
 
 
 class QLogHandler(logging.Handler):
