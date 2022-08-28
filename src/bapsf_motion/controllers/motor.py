6b962de7768d32944ab843876be8e054d1ec2d1e
Erik <eteverson@gmail.com>
2022-08-28 16:16:17 -0700

    create SimpleSignal class

--- src/bapsf_motion/controllers/motor.py
+++ src/bapsf_motion/controllers/motor.py
@@ -17,6 +17,30 @@ _logger = logging.getLogger(__name__)
 _ipv4_pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 
 
+class SimpleSignal:
+    _handlers = None
+
+    @property
+    def handlers(self):
+        if self._handlers is None:
+            self._handlers = []
+        return self._handlers
+
+    def connect(self, func):
+        if func not in self.handlers:
+            self.handlers.append(func)
+
+    def disconnect(self, func):
+        try:
+            self.handlers.remove(func)
+        except ValueError:
+            pass
+
+    def emit(self, payload):
+        for handler in self.handlers:
+            handler(payload)
+
+
 class Motor:
     logger = None
     name = ""
