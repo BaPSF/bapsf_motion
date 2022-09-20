877abb403d60d606238f4f17c4bec3b487dc7385
Erik <eteverson@gmail.com>
2022-09-20 08:54:05 -0700

    blank file for the Manager class

--- src/bapsf_motion/actors/__init__.py
+++ src/bapsf_motion/actors/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ["Axis", "Drive", "Motor"]
+__all__ = ["Axis", "Drive", "Manager", "MotionGroup", "Motor"]
 
 from bapsf_motion.actors.axis_ import Axis
 from bapsf_motion.actors.drive_ import Drive
+from bapsf_motion.actors.manager_ import Manager
+from bapsf_motion.actors.motion_group_ import MotionGroup
 from bapsf_motion.actors.motor_ import Motor
