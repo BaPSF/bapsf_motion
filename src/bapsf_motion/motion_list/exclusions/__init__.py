d606a9bb5b8d8245406887a6bb31f33cc839a028
Erik <eteverson@gmail.com>
2022-10-11 16:48:44 -0700

    create DividerExclusion

--- src/bapsf_motion/motion_list/exclusions/__init__.py
+++ src/bapsf_motion/motion_list/exclusions/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["BaseExclusion", "CircularExclusion"]
+__all__ = ["BaseExclusion", "CircularExclusion", "DividerExclusion"]
 
 from bapsf_motion.motion_list.exclusions.base import BaseExclusion
 from bapsf_motion.motion_list.exclusions.circular import CircularExclusion
+from bapsf_motion.motion_list.exclusions.divider import DividerExclusion
 
 # TODO: types of exclusions
 #       - Divider (greater/less than a dividing line)
