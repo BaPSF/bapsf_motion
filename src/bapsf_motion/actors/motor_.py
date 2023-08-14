fce0f5579366628c01337fb687f76e7a74446abc
Erik <eteverson@gmail.com>
2023-08-14 16:50:31 -0700

    first take at an inclusive move_off_limit

--- src/bapsf_motion/actors/motor_.py
+++ src/bapsf_motion/actors/motor_.py
@@ -1316,6 +1316,55 @@ class Motor(BaseActor):
         off_direction = -1 if self.status["limits"]["CW"] else 1
 
         counts = 1
+        on_limits = any(self.status["limits"].values())
+        while on_limits:
+
+            # off_direction = -1 if self.status["limits"]["CW"] else 1
+            pos = self._send_command("get_position").value
+            move_to_pos = pos + off_direction * 0.5 * self.steps_per_rev.value
+
+            self.logger.warning("disable limits")
+            self._send_raw_command("DL3")
+            self.send_command("alarm_reset")
+            # self.sleep(2 * self.heartrate.active)
+
+            self.move_to(move_to_pos)
+
+            self.logger.warning("enable limits")
+            self._send_raw_command("DL1")
+            self.sleep(2 * self.heartrate.active)
+
+            alarm_msg = self.retrieve_motor_alarm(defer_status_update=True)
+            on_limits = any(alarm_msg["limits"].values())
+
+            # future = asyncio.run_coroutine_threadsafe(
+            #     self._move_off_limit_async(off_direction),
+            #     self._loop,
+            # )
+            # future.result(5)
+
+            if counts > 10:
+                self.logger.error("Was not able to move of limit.")
+                break
+
+            self.logger.debug(f"Limit status is {self.status['limits']}.")
+            counts += 1
+
+    def _move_off_limit(self):
+
+        # are we on a limit
+        if not any(self.status["limits"].values()):
+            self.logger.debug(f"Motor is NOT on a limit, doing nothing.")
+            return
+        elif all(self.status["limits"].values()):
+            self.logger.error(
+                "Both CW and CCW limits activated, can not do anything."
+            )
+            return
+
+        off_direction = -1 if self.status["limits"]["CW"] else 1
+
+        counts = 1
         while any(self.status["limits"].values()):
 
             off_direction = -1 if self.status["limits"]["CW"] else 1
