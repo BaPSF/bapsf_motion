a187945d42816b808313321feb2c13fdb024eb34
Erik <eteverson@gmail.com>
2023-10-06 13:04:02 -0700

    first attempt at an EventActor abstract class

--- src/bapsf_motion/actors/base.py
+++ src/bapsf_motion/actors/base.py
@@ -5,10 +5,12 @@ Module for functionality focused around the [Abstract] base actors.
 __all__ = ["BaseActor"]
 __actors__ = ["BaseActor"]
 
+import asyncio
 import logging
+import threading
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional, Union
 
 
 # TODO: create an EventActor for an actor that utilizes asyncio event loops
@@ -99,3 +101,148 @@ class BaseActor(ABC):
 #       - will likely need abstract methods _actor_setup_pre_loop() and
 #         _actor_setup_post_loop() for setup actions before and after
 #         the loop creation, respectively.
+
+
+class EventActor(BaseActor, ABC):
+    def __init__(
+        self,
+        *,
+        name: str = None,
+        logger: logging.Logger = None,
+        loop: asyncio.AbstractEventLoop = None,
+        auto_run: bool = False,
+    ):
+
+        super().__init__(name=name, logger=logger)
+
+        self._thread = None
+        self._loop = self.setup_event_loop(loop)
+        self._tasks = None
+
+        self._configure_before_run()
+        self._initialize_tasks()
+
+        self.run(auto_run)
+
+    @property
+    def tasks(self) -> List[asyncio.Task]:
+        r"""
+        List of `asyncio.Task`\ s this actor has in its `event loop`_.
+        """
+        if self._tasks is None:
+            self._tasks = []
+
+        return self._tasks
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        """The `asyncio` :term:`event loop` for the actor."""
+        return self._loop
+
+    @property
+    def thread(self) -> threading.Thread:
+        """The `~threading.Thread` the `event loop`_ is running in."""
+        return self._thread
+
+    @property
+    def _thread_id(self) -> Union[int, None]:
+        """Unique ID for the thread the loop is running in."""
+        if self.loop is None or not self.loop.is_running():
+            # no loop has been created or loop is not running
+            return None
+
+        # get ident from running loop
+
+        # return self.loop._thread_id if self._thread is None else self.thread.ident
+
+        future = asyncio.run_coroutine_threadsafe(
+            self._thread_id_async(),
+            self._loop
+        )
+        return future.result(5)
+
+    async def _thread_id_async(self):
+        return threading.current_thread().ident
+
+    @abstractmethod
+    def _configure_before_run(self):
+        ...
+
+    @abstractmethod
+    def _initialize_tasks(self):
+        ...
+
+    def setup_event_loop(self, loop: Optional[asyncio.AbstractEventLoop]):
+        """
+        Set up the `asyncio` `event loop`_.  If the given loop is not an
+        instance of `~asyncio.AbstractEventLoop`, then a new loop will
+        be created.
+
+        Parameters
+        ----------
+        loop: `asyncio.AbstractEventLoop`
+            `asyncio` `event loop`_ for the actor's tasks
+
+        """
+        # 1. loop is given and running
+        #    - store loop
+        # 2. loop is given and not running
+        #    - store loop
+        # 3. loop is NOT given
+        #    - create new loop
+        #    - store loop
+        # get a valid event loop
+        if loop is None:
+            loop = asyncio.new_event_loop()
+        elif not isinstance(loop, asyncio.AbstractEventLoop):
+            self.logger.warning(
+                "Given asyncio event is not valid.  Creating a new event loop to use."
+            )
+            loop = asyncio.new_event_loop()
+        return loop
+
+    def run(self, auto_run=True):
+        """
+        Activate the `asyncio` `event loop`_.   If the event loop is
+        running, then nothing happens.  Otherwise, the event loop is
+        placed in a separate thread and set to
+        `~asyncio.loop.run_forever`.
+        """
+        if self._loop.is_running():
+            return
+        elif not auto_run:
+            return
+
+        _thread = threading.Thread(target=self._loop.run_forever)
+        _thread.start()
+        self._thread = _thread
+
+    def terminate(self, delay_loop_stop=False):
+        r"""
+        Stop the actor's `event loop`_\ .  All actor tasks will be
+        cancelled, the connection to the motor will be shutdown, and
+        the event loop will be stopped.
+
+        Parameters
+        ----------
+        delay_loop_stop: bool
+            If `True`, then do NOT stop the `event loop`_\ .  In this
+            case it is assumed the calling functionality is managing
+            additional tasks in the event loop, and it is up to that
+            functionality to stop the loop.  (DEFAULT: `False`)
+
+        """
+        # TODO: add additional motor shutdown tasks (i.e. stop and disable)
+        for task in list(self.tasks):
+            task.cancel()
+            self.tasks.remove(task)
+
+        # try:
+        #     self.socket.close()
+        # except AttributeError:
+        #     pass
+
+        if delay_loop_stop:
+            return
+
+        self._loop.call_soon_threadsafe(self._loop.stop)
