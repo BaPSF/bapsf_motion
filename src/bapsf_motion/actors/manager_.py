5d07646949efdc47a885a5ae28e5cecf1fa5416c
Erik <eteverson@gmail.com>
2023-10-09 16:54:35 -0700

    add configuration validation to RunManager

--- src/bapsf_motion/actors/manager_.py
+++ src/bapsf_motion/actors/manager_.py
@@ -8,17 +8,22 @@ __actors__ = ["RunManager"]
 
 import asyncio
 import logging
+import warnings
 
 from collections import UserDict
+from datetime import datetime, timezone
 from typing import Any, Dict, List, Union
 
 from bapsf_motion.actors.base import EventActor
 from bapsf_motion.actors.motion_group_ import MotionGroup, MotionGroupConfig
 from bapsf_motion.utils import toml
+from bapsf_motion.utils.exceptions import ConfigurationWarning
 
 
 class RunManagerConfig(UserDict):
     _manager_names = {"run"}
+    _mg_names = MotionGroupConfig._mg_names
+    _required_metadata = {"run", "name"}
 
     def __init__(self, config: Union[str, Dict[str, Any]]):
 
@@ -58,20 +63,132 @@ class RunManagerConfig(UserDict):
 
         # validate config
         config = self._validate_config(config)
-        self._drive = None
-        self._transform = None
-        self._motion_builder = None
+        self._mgs = None
 
         super().__init__(config)
         self._data = self.data
 
+    @property
+    def data(self):
+        """
+        A real dictionary used to store the contents of
+        `RunManagerConfig`.
+        """
+        if self._mgs is not None:
+            self._data = {**self._data, "motion_group": self._mgs}
+
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
     def _validate_config(self, config):
+        date = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M %Z')
+        if "name" not in config:
+            rname = f"run [{date}]"
+            warnings.warn(
+                "Run configuration is missing a unique name for the run, "
+                f"naming the configuration '{rname}'",
+                ConfigurationWarning,
+            )
+            config["name"] = rname
+
+        config["date"] = date
+
+        # Are there motion groups
+        mg_names_not_in_config = self._mg_names - set(config)
+        if len(mg_names_not_in_config) == len(self._mg_names):
+            raise ValueError(
+                "The run configuration has no defined motion groups, "
+                "there needs to be at least one motion group."
+            )
+
+        # collect possible motion group configurations
+        mg_names_in_config = self._mg_names - mg_names_not_in_config
+        collected_mg_configs = {}
+        for mg_name in mg_names_in_config:
+            mg_config = config.pop(mg_name)
+
+            if not isinstance(mg_config, dict):
+                raise TypeError(
+                    "Expected a dictionary for the motion group configuration,"
+                    f" but got type {type(mg_config)}."
+                )
+
+            if "name" in mg_config:
+                # assume only one motion group is defined
+                index = len(collected_mg_configs)
+                collected_mg_configs[index] = mg_config
+                continue
+
+            for mgc in mg_config.values():
+
+                if not isinstance(mgc, dict):
+                    raise TypeError(
+                        "Expected a dictionary for the motion group configuration,"
+                        f" but got type {type(mg_config)}."
+                    )
+
+                index = len(collected_mg_configs)
+                collected_mg_configs[index] = mgc
+
+        config = self._handle_user_meta(config, {"name", "date"})
+        config["motion_group"] = {}
+
+        for key, val in collected_mg_configs.items():
+            config["motion_group"][key] = MotionGroupConfig(val)
+
+        self._validate_motion_group_names(config)
+        self._validate_drive_ips(config)
+
         return config
 
-    def _validate_motion_group(self, config):
-        ...
+    @staticmethod
+    def _validate_motion_group_names(config: Dict[str, Any]):
+        mg_config_names = [val["name"] for val in config["motion_group"].values()]
+        if len(set(mg_config_names)) != len(mg_config_names):
+            duplicates = []
+            for name in set(mg_config_names):
+                count = mg_config_names.count(name)
+                if count != 1:
+                    duplicates.append(name)
 
-    def link_motion_group(self, mg, index):
+            raise ValueError(
+                "All configured motion groups must have unique names, found"
+                f" duplicates for {duplicates}."
+            )
+
+    @staticmethod
+    def _validate_drive_ips(config: Dict[str, Any]):
+        drive_configs = [val["drive"] for val in config["motion_group"].values()]
+        drive_ips = []
+        for dr in drive_configs:
+            ips = [val["ip"] for val in dr["axes"].values()]
+            drive_ips.extend(ips)
+
+        if len(set(drive_ips)) != len(drive_ips):
+            duplicates = []
+            for ip in set(drive_ips):
+                count = drive_ips.count(ip)
+                if count != 1:
+                    duplicates.append(ip)
+
+            raise ValueError(
+                "All configured motion groups must have unique motor IP "
+                f"addresses, found  duplicates for {duplicates}."
+            )
+
+    @staticmethod
+    def _handle_user_meta(config: Dict[str, Any], req_meta: set) -> Dict[str, Any]:
+        """
+        If a user specifies metadata that is not required by a specific
+        configuration component, then collect all the metadata and
+        store it under the 'user' key.  Return the modified dictionary.
+        """
+        return MotionGroupConfig._handle_user_meta(config, req_meta)
+
+    def link_motion_group(self, mg, key):
         ...
 
 
