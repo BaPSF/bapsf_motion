05e8e01b247bddce5e7c6d7e705fd84cfddaa37f
Erik <eteverson@gmail.com>
2023-10-09 16:39:52 -0700

    add an examples loader `load_example()` to bapsf_motion.utils

--- src/bapsf_motion/utils/__init__.py
+++ src/bapsf_motion/utils/__init__.py
@@ -3,6 +3,7 @@ __all__ = [
     "steps",
     "rev",
     "ipv4_pattern",
+    "load_example",
     "units",
     "SimpleSignal",
     "toml",
@@ -10,9 +11,12 @@ __all__ = [
 import re
 
 from astropy import units
+from pathlib import Path
 
 from bapsf_motion.utils import exceptions, toml
 
+_HERE = Path(__file__).resolve().parent
+_EXAMPLES = (_HERE / ".." / "examples").resolve()
 
 #: Regular expression pattern for parsing IPv4 addresses
 ipv4_pattern = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
@@ -52,3 +56,41 @@ class SimpleSignal:
     def emit(self, payload):
         for handler in self.handlers:
             handler(payload)
+
+
+def load_example(filename: str, as_string=False):
+    """
+    Load an example TOML file from `bapsf_motion.examples`.
+
+    Parameters
+    ----------
+    filename: `str`
+        Name of the example file, including extension.
+
+    as_string: `bool`, optional
+        If `True`, then return the example configuration as a TOML
+        string; otherwise, return the configuration as a dictionary.
+        (DEFAULT:`False`)
+
+    Returns
+    -------
+    config: `dict` or `str`
+        Return the example configuration.
+    """
+    _file = (_EXAMPLES / filename).resolve()
+
+    if not _file.exists():
+        raise ValueError(
+            f"The specified example file {filename} does not exist in "
+            f"the examples directory {_EXAMPLES}."
+        )
+    elif not _file.is_file():
+        raise ValueError(f"The specified example file {filename} is not a file.")
+
+    with open(_file, "rb") as f:
+        config = toml.load(f)
+
+    if as_string:
+        config = toml.dumps(config)
+
+    return config
