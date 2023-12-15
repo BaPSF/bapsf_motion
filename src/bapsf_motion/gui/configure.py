dcc81dcfc8f6b70cffdf7694587a46665f706b8f
Erik <eteverson@gmail.com>
2023-12-15 11:28:41 -0800

    create class RunWidget

--- src/bapsf_motion/gui/configure.py
+++ src/bapsf_motion/gui/configure.py
@@ -10,10 +10,82 @@ from PySide6.QtWidgets import (
     QWidget,
     QSizePolicy,
     QFrame,
+    QPushButton,
+    QTextEdit,
+    QListWidget,
+    QVBoxLayout,
 )
 from typing import Any, Dict
 
 from bapsf_motion.gui.widgets import QLogger
+
+
+class RunWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.done_btn = QPushButton("DONE")
+        self.quit_btn = QPushButton("Discard & Quit")
+
+        self.import_btn = QPushButton("IMPORT")
+        self.export_btn = QPushButton("EXPORT")
+        self.add_mg_btn = QPushButton("ADD")
+        self.remove_mg_btn = QPushButton("REMOVE")
+        self.modify_mg_btn = QPushButton("Edit / Control")
+
+        self.config_widget = QTextEdit()
+        self.mg_list_widget = QListWidget()
+
+        self.setLayout(self._define_layout())
+
+    def _define_layout(self):
+        main_layout = QVBoxLayout()
+
+        # Create layout for top buttons
+        top_btn_layout = QHBoxLayout()
+        top_btn_layout.addWidget(self.quit_btn)
+        top_btn_layout.addStretch()
+        top_btn_layout.addWidget(self.done_btn)
+
+        # Create layout for toml window
+        toml_layout = QGridLayout()
+        label = QLabel("Run Configuration")
+        label.setAlignment(
+            Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignBottom
+        )
+        label.font().setPointSize(12)
+
+        self.config_widget.setSizePolicy(
+            QSizePolicy.Policy.Preferred,
+            QSizePolicy.Policy.Expanding,
+        )
+        self.config_widget.setReadOnly(True)
+        self.config_widget.font().setPointSize(14)
+        self.config_widget.font().setFamily("Courier New")
+
+        toml_layout.addWidget(label, 0, 0, 1, 2)
+        toml_layout.addWidget(self.config_widget, 1, 0, 1, 2)
+        toml_layout.addWidget(self.import_btn, 2, 0, 1, 1)
+        toml_layout.addWidget(self.export_btn, 2, 1, 1, 1)
+
+        toml_widget = QWidget()
+        toml_widget.setLayout(toml_layout)
+        toml_widget.setMinimumWidth(400)
+        toml_widget.setMinimumWidth(500)
+        toml_widget.sizeHint().setWidth(450)
+
+        # Create layout for
+
+        # Construct layout below top buttons
+        layout = QHBoxLayout()
+        layout.addWidget(toml_widget)
+        layout.addStretch()
+
+        # Populate the main layout
+        main_layout.addLayout(top_btn_layout)
+        main_layout.addLayout(layout)
+
+        return main_layout
 
 
 class ConfigureGUI(QMainWindow):
