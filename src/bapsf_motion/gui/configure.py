2698b9d59824afbcedebe0bba0a759bcb493af9b
Erik <eteverson@gmail.com>
2024-01-05 16:41:25 -0800

    create _OverlayWidget and DriveConfigOverlay widgets

--- src/bapsf_motion/gui/configure.py
+++ src/bapsf_motion/gui/configure.py
@@ -3,7 +3,7 @@ import logging.config
 
 from pathlib import Path
 from PySide6.QtCore import Qt, QDir, Signal
-from PySide6.QtGui import QCloseEvent
+from PySide6.QtGui import QCloseEvent, QColor, QPainter
 from PySide6.QtWidgets import (
     QMainWindow,
     QHBoxLayout,
@@ -24,6 +24,97 @@ from typing import Any, Dict, Union
 from bapsf_motion.actors import RunManager
 from bapsf_motion.gui.widgets import QLogger, StyleButton
 from bapsf_motion.utils import toml
+
+
+class _OverlayWidget(QWidget):
+    closing = Signal()
+
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+
+        # make the window frameless
+        # self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.setStyleSheet("border: 2px solid black")
+
+        self.background_fill_color = QColor(30, 30, 30, 120)
+        self.background_pen_color = QColor(30, 30, 30, 120)
+
+        self.overlay_fill_color = QColor(50, 50, 50)
+        self.overlay_pen_color = QColor(90, 90, 90)
+
+        self._margins = [0.02, 0.04]
+        self._set_contents_margins(*self._margins)
+
+    def _set_contents_margins(self, width_fraction, height_fraction):
+        width = int(width_fraction * self.parent().width())
+        height = int(height_fraction * self.parent().height())
+
+        self.setContentsMargins(width, height, width, height)
+
+    def paintEvent(self, event):
+        # This method is, in practice, drawing the contents of
+        # your window.
+
+        # get current window size
+        s = self.parent().size()
+        qp = QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QPainter.RenderHint.Antialiasing, True)
+        qp.setPen(self.background_pen_color)
+        qp.setBrush(self.background_fill_color)
+        qp.drawRoundedRect(0, 0, s.width(), s.height(), 3, 3)
+
+        # draw overlay
+        qp.setPen(self.overlay_pen_color)
+        qp.setBrush(self.overlay_fill_color)
+
+        self.contentsRect().width()
+        ow = int((s.width() - self.contentsRect().width()) / 2)
+        oh = int((s.height() - self.contentsRect().height()) / 2)
+        qp.drawRoundedRect(
+            ow,
+            oh,
+            self.contentsRect().width(),
+            self.contentsRect().height(),
+            5,
+            5,
+        )
+
+        qp.end()
+
+    def closeEvent(self, event):
+        self.closing.emit()
+        event.accept()
+
+
+class DriveConfigOverlay(_OverlayWidget):
+
+    def __init__(self, parent: "MGWidget" = None):
+        super().__init__(parent)
+
+        _btn = StyleButton(parent=self)
+        _btn.setText("X")
+        font = _btn.font()
+        font.setPixelSize(18)
+        font.setBold(True)
+        _btn.setFont(font)
+        # _btn.setStyleSheet("background-color: rgb(0, 0, 0, 0)")
+        _btn.setFixedSize(30, 30)
+        self.close_btn = _btn
+
+        self.setLayout(self._define_layout())
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self._connect_signals()
+
+    def _define_layout(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.close_btn)
+        return layout
+
+    def _connect_signals(self):
+        self.close_btn.clicked.connect(self.close)
 
 
 class RunWidget(QWidget):
