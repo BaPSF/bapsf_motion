f30ad29aaa4e69f1b74f7aedf5482620e0b960d5
Erik <eteverson@gmail.com>
2024-02-01 11:01:20 -0800

    create custom widgets HLinePlain and VLinePlain

--- src/bapsf_motion/gui/widgets/__init__.py
+++ src/bapsf_motion/gui/widgets/__init__.py
@@ -6,8 +6,15 @@ __all__ = [
     "StyleButton",
     "IPv4Validator",
     "QLineEditeSpecialized",
+    "HLinePlain",
+    "VLinePlain",
 ]
 
 from bapsf_motion.gui.widgets.logging import QLogHandler, QLogger
 from bapsf_motion.gui.widgets.buttons import LED, StopButton, StyleButton
-from bapsf_motion.gui.widgets.misc import IPv4Validator, QLineEditeSpecialized
+from bapsf_motion.gui.widgets.misc import (
+    IPv4Validator,
+    QLineEditeSpecialized,
+    HLinePlain,
+    VLinePlain,
+)
--- src/bapsf_motion/gui/widgets/misc.py
+++ src/bapsf_motion/gui/widgets/misc.py
@@ -1,10 +1,15 @@
-__all__ = ["IPv4Validator", "QLineEditeSpecialized"]
+__all__ = [
+    "IPv4Validator",
+    "QLineEditeSpecialized",
+    "HLinePlain",
+    "VLinePlain",
+]
 
 import logging
 
 from PySide6.QtCore import Signal
-from PySide6.QtGui import QValidator
-from PySide6.QtWidgets import QLineEdit
+from PySide6.QtGui import QValidator, QColor
+from PySide6.QtWidgets import QLineEdit, QFrame,QWidget
 
 from bapsf_motion.utils import ipv4_pattern as _ipv4_pattern
 
@@ -40,3 +45,56 @@ class QLineEditeSpecialized(QLineEdit):
     def _send_payload(self):
         self.editingFinishedPayload.emit(self)
 
+
+class HLinePlain(QFrame):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent=parent)
+
+        self.setFrameShape(QFrame.Shape.HLine)
+        self.setFrameShadow(QFrame.Shadow.Plain)
+        self.setLineWidth(3)
+        self.setMidLineWidth(3)
+
+        self.set_color(125, 125, 125)
+
+    def setLineWidth(self, arg__1: int):
+        super().setLineWidth(arg__1)
+        if self.lineWidth() != self.midLineWidth():
+            self.setMidLineWidth(arg__1)
+
+    def setMidLineWidth(self, arg__1: int):
+        super().setMidLineWidth(arg__1)
+        if self.lineWidth() != self.midLineWidth():
+            self.setLineWidth(arg__1)
+
+    def set_color(self, r: int, g: int, b: int):
+        palette = self.palette()
+        palette.setColor(palette.ColorRole.WindowText, QColor(r, g, b))
+        self.setPalette(palette)
+
+
+class VLinePlain(QFrame):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent=parent)
+
+        self.setFrameShape(QFrame.Shape.VLine)
+        self.setFrameShadow(QFrame.Shadow.Plain)
+        self.setLineWidth(3)
+        self.setMidLineWidth(3)
+
+        self.set_color(125, 125, 125)
+
+    def setLineWidth(self, arg__1: int):
+        super().setLineWidth(arg__1)
+        if self.lineWidth() != self.midLineWidth():
+            self.setMidLineWidth(arg__1)
+
+    def setMidLineWidth(self, arg__1: int):
+        super().setMidLineWidth(arg__1)
+        if self.lineWidth() != self.midLineWidth():
+            self.setLineWidth(arg__1)
+
+    def set_color(self, r: int, g: int, b: int):
+        palette = self.palette()
+        palette.setColor(palette.ColorRole.WindowText, QColor(r, g, b))
+        self.setPalette(palette)
