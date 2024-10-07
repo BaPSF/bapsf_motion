3ba142a7ba258e8552c28b8a5cbfe5aa89b8993e
Erik <eteverson@gmail.com>
2024-10-07 15:27:33 -0700

    expose droop functionality to the bapsf_motion.transform namespace

--- bapsf_motion/transform/__init__.py
+++ bapsf_motion/transform/__init__.py
@@ -2,6 +2,8 @@ __all__ = [
     "transform_factory",
     "register_transform",
     "BaseTransform",
+    "DroopCorrectABC",
+    "LaPDXYDroopCorrect",
 ]
 __transformer__ = ["IdentityTransform", "LaPDXYTransform"]
 __all__ += __transformer__
@@ -10,3 +12,4 @@ from bapsf_motion.transform.base import BaseTransform
 from bapsf_motion.transform.helpers import register_transform, transform_factory
 from bapsf_motion.transform.lapd import LaPDXYTransform
 from bapsf_motion.transform.identity import IdentityTransform
+from bapsf_motion.transform.lapd_droop import DroopCorrectABC, LaPDXYDroopCorrect
