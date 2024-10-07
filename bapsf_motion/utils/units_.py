0f52ed1f21c08e216eabe76cf142444226963e94
Erik <eteverson@gmail.com>
2024-10-07 11:02:13 -0700

    move unit functionality into its own module

---
+++ bapsf_motion/utils/units_.py
@@ -0,0 +1,17 @@
+__all__ = ["units", "counts", "steps", "rev"]
+from astropy import units
+
+# enable imperial units
+units.imperial.enable()
+
+#: Base unit for encoders.
+counts = units.def_unit("counts", namespace=units.__dict__)
+
+#: Base unit for stepper motors.
+steps = units.def_unit("steps", namespace=units.__dict__)
+
+#: An unit for the instance of revolving.
+rev = units.def_unit("rev", namespace=units.__dict__)
+
+for _u in {counts, steps, rev}:
+    units.add_enabled_units(_u)
