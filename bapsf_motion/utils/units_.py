944474e414401a85bde3047ee87e3a6a3f869b2e
Erik <eteverson@gmail.com>
2024-10-08 10:23:34 -0700

    add docstrings for units counts, steps, rev

--- bapsf_motion/utils/units_.py
+++ bapsf_motion/utils/units_.py
@@ -5,14 +5,26 @@ from astropy import units
 # enable imperial units
 units.imperial.enable()
 
-#: Base unit for encoders.
-counts = units.def_unit("counts", namespace=units.__dict__)
+#: Base unit for (stepper motor) encoders.
+counts = units.def_unit(
+    "counts",
+    namespace=units.__dict__,
+    doc="Base unit for (stepper motor) encoders.",
+)
 
 #: Base unit for stepper motors.
-steps = units.def_unit("steps", namespace=units.__dict__)
+steps = units.def_unit(
+    "steps",
+    namespace=units.__dict__,
+    doc="Base unit for stepper motors.",
+)
 
-#: An unit for the instance of revolving.
-rev = units.def_unit("rev", namespace=units.__dict__)
+#: A unit for the instance of revolving.
+rev = units.def_unit(
+    "rev",
+    namespace=units.__dict__,
+    doc="A unit for the instance of revolving.",
+)
 
 for _u in {counts, steps, rev}:
     units.add_enabled_units(_u)
