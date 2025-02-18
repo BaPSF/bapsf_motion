8c847310f34a624d8ae5b78f7180b5e194aa5a05
Erik <eteverson@gmail.com>
2025-02-18 13:52:28 -0800

    create IconButton

--- bapsf_motion/gui/widgets/buttons.py
+++ bapsf_motion/gui/widgets/buttons.py
@@ -5,6 +5,7 @@ __all__ = [
     "DoneButton",
     "GearButton",
     "GearValidButton",
+    "IconButton",
     "LED",
     "StopButton",
     "StyleButton",
@@ -13,9 +14,9 @@ __all__ = [
 import math
 
 from PySide6.QtCore import QSize
-from PySide6.QtGui import QFontMetrics, QColor
+from PySide6.QtGui import QFontMetrics, QColor, QIcon
 from PySide6.QtWidgets import QPushButton
-from typing import Optional
+from typing import Optional, Union
 
 # noqa
 # import of qtawesome must happen after the PySide6 imports
@@ -156,6 +157,54 @@ class StyleButton(QPushButton):
             self._disabled_style = {**_style, **styles}
 
         self._resetStyleSheet()
+
+
+class IconButton(StyleButton):
+    def __init__(
+        self,
+        qta_icon_name: str,
+        *args,
+        color: Union[QColor, str, None] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        try:
+            color = cast_color_to_rgba_string(color)
+        except (ValueError, TypeError):
+            color = None
+
+        if color is None:
+            _palette = self.palette()
+            _palette_color = _palette.color(_palette.ColorRole.ButtonText)
+
+            color = self.base_style.get("color", _palette_color)
+
+        if not isinstance(color, QColor):
+            color = cast_color_to_rgba_string(color)
+            icon_color = color.replace("rgba(", "").replace(")", "")
+            r, g, b, a = map(int, icon_color.split(","))
+            icon_color = QColor(r, g, b, a=a)
+        else:
+            icon_color = color
+
+        self.update_style_sheet(styles={"color": color}, action="base")
+        self._icon = qta.icon(qta_icon_name, color=icon_color)
+
+        self.setIcon(self._icon)
+
+    @property
+    def icon(self) -> QIcon:
+        return self._icon
+
+    def setIconSize(self, size: Union[QSize, int]):
+        if isinstance(size, int):
+            size = QSize(size, size)
+
+        if not isinstance(size, QSize):
+            return
+
+        super().setIconSize(size)
 
 
 class BannerButton(StyleButton):
