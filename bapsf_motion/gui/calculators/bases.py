e0091a5453cd08c37fe35d64b112a3365ebce8fd
erik <eteverson@gmail.com>
2026-05-22 11:11:20 -0700

    create calculator base classes BaseCalculatorWindow and BaseCalculatorApp

---
+++ bapsf_motion/gui/calculators/bases.py
@@ -0,0 +1,31 @@
+__all__ = ["BaseCalculatorApp", "BaseCalculatorWindow"]
+from abc import ABC, abstractmethod
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtWidgets import QApplication, QMainWindow
+from typing import Type
+
+
+class BaseCalculatorWindow(QMainWindow):
+    ...
+
+
+class BaseCalculatorApp(QApplication):
+    _CALCULATOR_CLASS = NotImplemented  # type: Type[BaseCalculatorWindow]
+
+    def __init__(self, *args, **kwargs):
+
+        if not issubclass(self._CALCULATOR_CLASS, BaseCalculatorWindow):
+            raise TypeError(
+                f"The defined class attribute _CALCULATOR_CLASS is not "
+                f"a subclass of {BaseCalculatorWindow.__module__}."
+                f"{BaseCalculatorWindow.__name__}"
+            )
+
+        super().__init__(*args, **kwargs)
+
+        self.setStyle("Fusion")
+        self.styleHints().setColorScheme(Qt.ColorScheme.Light)
+
+        self._window = self._CALCULATOR_CLASS()
+        self._window.show()
+        self._window.activateWindow()
