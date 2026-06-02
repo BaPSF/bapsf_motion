774f5bdaf614a20ed4de6e73e6c1f47a981308b6
erik <eteverson@gmail.com>
2026-06-02 13:44:47 -0700

    import get_color_scheme() from bapsf_qt

--- bapsf_motion/gui/helpers.py
+++ bapsf_motion/gui/helpers.py
@@ -5,15 +5,4 @@ implementation of the package GUIs.
 
 __all__ = ["get_qapplication", "get_color_scheme", "cast_color_to_rgba_string"]
 
-from bapsf_qt.utils import cast_color_to_rgba_string, get_qapplication
-from PySide6.QtCore import Qt
-
-
-def get_color_scheme() -> "Qt.ColorScheme":
-    """
-    Retrieve the color scheme (light or dark mode) of the operating
-    system.  The returns the `~PySide6.QtCore.Qt.ColorScheme` `enum`.
-    """
-    app = get_qapplication()
-    _scheme = app.styleHints().colorScheme()
-    return _scheme
+from bapsf_qt.utils import cast_color_to_rgba_string, get_color_scheme, get_qapplication
