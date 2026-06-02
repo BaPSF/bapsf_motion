9b57f96c2d5546e80de574ff27ca53b4754711db
erik <eteverson@gmail.com>
2026-06-02 16:24:24 -0700

    import GearButton and IconButton from bapsf_qt

--- bapsf_motion/gui/widgets/buttons.py
+++ bapsf_motion/gui/widgets/buttons.py
@@ -21,6 +21,8 @@ from bapsf_qt.buttons import (
     AutoScaleButton,
     DiscardButton,
     DoneButton,
+    GearButton,
+    IconButton,
     StyleButton,
 )
 from PySide6.QtCore import QSize, Slot
@@ -33,89 +35,6 @@ from bapsf_motion.gui.icons import icon_name_dict
 
 # import of qtawesome must happen after the PySide6 imports
 import qtawesome as qta  # noqa
-
-
-class IconButton(StyleButton):
-    def __init__(
-        self,
-        qta_icon_name: str,
-        *args,
-        color: Union[QColor, str, None] = None,
-        **kwargs,
-    ):
-        super().__init__(*args, **kwargs)
-
-        try:
-            color = cast_color_to_rgba_string(color)
-        except (ValueError, TypeError):
-            color = None
-
-        if color is None:
-            _palette = self.palette()
-            _palette_color = _palette.color(_palette.ColorRole.ButtonText)
-
-            color = self.base_style.get("color", _palette_color)
-
-        if not isinstance(color, QColor):
-            color = cast_color_to_rgba_string(color)
-            icon_color = color.replace("rgba(", "").replace(")", "")
-            r, g, b, a = map(int, icon_color.split(","))
-            icon_color = QColor(r, g, b, a=a)
-        else:
-            icon_color = color
-
-        self.update_style_sheet(styles={"color": color}, action="base")
-        self._icon = qta.icon(qta_icon_name, color=icon_color)
-
-        self.setIcon(self._icon)
-
-    @property
-    def icon(self) -> QIcon:
-        return self._icon
-
-    def setIconSize(self, size: Union[QSize, int]):
-        if isinstance(size, int):
-            size = QSize(size, size)
-
-        if not isinstance(size, QSize):
-            return
-
-        super().setIconSize(size)
-
-
-class GearButton(StyleButton):
-    def __init__(self, color: Optional[str] = None, parent=None):
-        super().__init__(parent=parent)
-
-        try:
-            color = cast_color_to_rgba_string(color)
-        except (ValueError, TypeError):
-            color = None
-
-        if color is None:
-            _palette = self.palette()
-            _palette_color = _palette.color(_palette.ColorRole.ButtonText)
-
-            color = self.base_style.get("color", _palette_color)
-
-        if not isinstance(color, QColor):
-            color = cast_color_to_rgba_string(color)
-            icon_color = color.replace("rgba(", "").replace(")", "")
-            r, g, b, a = map(int, icon_color.split(","))
-            icon_color = QColor(r, g, b, a=a)
-        else:
-            icon_color = color
-
-        self.update_style_sheet(styles={"color": color}, action="base")
-        self._icon = qta.icon(icon_name_dict["gear"], color=icon_color)
-        self.setIcon(self._icon)
-
-        self._size = 32
-        self._icon_size = 24
-
-        self.setFixedWidth(self._size)
-        self.setFixedHeight(self._size)
-        self.setIconSize(QSize(self._icon_size, self._icon_size))
 
 
 class ValidButton(StyleButton):
