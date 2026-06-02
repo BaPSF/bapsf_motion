52de090cd4faac4890355492c3397dbb5bc37d34
erik <eteverson@gmail.com>
2026-06-02 13:35:16 -0700

    import get_qapplication() from bapsf_qt

--- bapsf_motion/gui/helpers.py
+++ bapsf_motion/gui/helpers.py
@@ -5,22 +5,8 @@ implementation of the package GUIs.
 
 __all__ = ["get_qapplication", "get_color_scheme", "cast_color_to_rgba_string"]
 
-import ast
-
-from bapsf_qt.utils import cast_color_to_rgba_string
+from bapsf_qt.utils import cast_color_to_rgba_string, get_qapplication
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication
-from typing import Union
-
-
-def get_qapplication() -> Union[QApplication, None]:
-    """
-    Get the current active instance of
-    `~PySide6.QtWidgets.QApplication`.  This is a convinces function
-    to `~PySide6.QtCore.QCoreApplication.instance`.
-    """
-    app = QApplication.instance()
-    return app
 
 
 def get_color_scheme() -> "Qt.ColorScheme":
