912ac4de253993d94e9710a8a8fc54b79a116417
erik <eteverson@gmail.com>
2026-06-02 17:43:59 -0700

    import IPv4Validator from bapsf_qt

--- bapsf_motion/gui/widgets/misc.py
+++ bapsf_motion/gui/widgets/misc.py
@@ -12,15 +12,12 @@ __all__ = [
 import logging
 
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QValidator
-from PySide6.QtWidgets import QLabel, QLineEdit
-
-from bapsf_motion.utils import ipv4_pattern as _ipv4_pattern
+from PySide6.QtWidgets import QLabel
 
 # import of qtawesome and bapsf_qt must happen after the PySide6 imports
 import qtawesome as qta  # noqa
 from bapsf_qt.widgets import QLineEditPayload as QLineEditSpecialized  # noqa
-from bapsf_qt.widgets import HLinePlain, VLinePlain, QTAIconLabel  # noqa
+from bapsf_qt.widgets import HLinePlain, IPv4Validator, VLinePlain, QTAIconLabel  # noqa
 
 
 class BatteryStatusIcon(QLabel):
@@ -52,23 +49,3 @@ class BatteryStatusIcon(QLabel):
             _icon = self._icon_map["unknown"]
 
         self.setPixmap(_icon.pixmap(self._pixmap_size, self._pixmap_size))
-
-
-class IPv4Validator(QValidator):
-    def __init__(self, logger=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._pattern = _ipv4_pattern
-
-        log_name = "" if logger is None else f"{logger.name}."
-        log_name += "IPv4Validator"
-        self._logger = logging.getLogger(log_name)
-
-    def validate(self, arg__1: str, arg__2: int) -> object:
-        string = arg__1.replace("_", "")
-
-        match = self._pattern.fullmatch(string)
-        if match is None:
-            self._logger.warning(f"IP address is invalid, '{string}'.")
-            return QValidator.State.Intermediate
-
-        return QValidator.State.Acceptable
