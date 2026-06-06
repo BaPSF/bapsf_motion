bd2a44eee672f589276021b2fe42022d41ab8afe
Erik Everson <eteverson@gmail.com>
2026-06-06 13:32:08 -0700

    remove unnecessary comment

--- bapsf_motion/gui/widgets/logging.py
+++ bapsf_motion/gui/widgets/logging.py
@@ -5,5 +5,4 @@ by Python's `logging` package.
 
 __all__ = ["QLogHandler", "QLogger"]
 
-# import of bapssf_qt must happen after the PySide6 imports
 from bapsf_qt.widgets import QLogHandler, QLogger  # noqa
