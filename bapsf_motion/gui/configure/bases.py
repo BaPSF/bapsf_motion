ddaf30a615bc31ddcca9c8fdfddfd989d0de5481
Erik <eteverson@gmail.com>
2026-06-18 11:20:03 -0700

    replace use of Union[] with pipe |

--- bapsf_motion/gui/configure/bases.py
+++ bapsf_motion/gui/configure/bases.py
@@ -15,7 +15,7 @@ from abc import ABC, ABCMeta, abstractmethod
 from PySide6.QtCore import Qt, Signal, Slot
 from PySide6.QtGui import QColor, QPainter, QPen
 from PySide6.QtWidgets import QSizePolicy, QWidget
-from typing import Any, Dict, Union
+from typing import Any, Dict
 
 from bapsf_motion.actors import MotionGroup
 from bapsf_motion.gui.configure import motion_group_widget as mgw
@@ -99,7 +99,7 @@ class _ConfigOverlay(_OverlayWidget, ABC, metaclass=_ABCMetaQWidget):
     returnConfig = Signal(object)
 
     def __init__(
-        self, mg: Union[MotionGroup, None], parent: "mgw.MGWidget | None" = None
+        self, mg: MotionGroup | None, parent: "mgw.MGWidget | None" = None
     ):
         super().__init__(parent=parent)
 
@@ -124,7 +124,7 @@ class _ConfigOverlay(_OverlayWidget, ABC, metaclass=_ABCMetaQWidget):
         return self._logger
 
     @property
-    def mg(self) -> Union[MotionGroup, None]:
+    def mg(self) -> MotionGroup | None:
         """Working motion group."""
         return self._mg
 
