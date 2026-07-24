6bdba41792745525baf07ab5f0f3e9fb621fa7d2
Erik <eteverson@gmail.com>
2026-07-24 13:54:48 -0700

    RunWidget: rename add_mg_btn -> mg_add_btn

--- bapsf_motion/gui/configure/configure_.py
+++ bapsf_motion/gui/configure/configure_.py
@@ -237,7 +237,7 @@ class RunWidget(QWidget):
         )
 
         # Initialize Widgets
-        self.add_mg_btn = self._init_add_mg_btn()
+        self.mg_add_btn = self._init_mg_add_btn()
         self.done_btn = self._init_done_btn()
         self.mg_list_widget = self._init_mg_list_widget()
         self.modify_mg_btn = self._init_modify_mg_btn()
@@ -307,7 +307,7 @@ class RunWidget(QWidget):
         layout.addWidget(self.mg_list_widget)
 
         sub_layout = QHBoxLayout()
-        sub_layout.addWidget(self.add_mg_btn)
+        sub_layout.addWidget(self.mg_add_btn)
         sub_layout.addWidget(self.remove_mg_btn)
         layout.addLayout(sub_layout)
 
@@ -315,7 +315,7 @@ class RunWidget(QWidget):
 
         return layout
 
-    def _init_add_mg_btn(self):
+    def _init_mg_add_btn(self):
         _btn = StyleButton("ADD", parent=self)
         _btn.setFixedHeight(38)
         _btn.setPointSize(16)
@@ -377,7 +377,7 @@ class RunWidget(QWidget):
 
     @Slot()
     def enable_mg_buttons(self):
-        self.add_mg_btn.setEnabled(True)
+        self.mg_add_btn.setEnabled(True)
         self.remove_mg_btn.setEnabled(True)
         self.modify_mg_btn.setEnabled(True)
 
@@ -478,7 +478,7 @@ class ConfigureGUI(QMainWindow):
         self._run_widget.done_btn.clicked.connect(self.save_and_close)
         self._run_widget.quit_btn.clicked.connect(self.discard_close)
 
-        self._run_widget.add_mg_btn.clicked.connect(self._motion_group_configure_new)
+        self._run_widget.mg_add_btn.clicked.connect(self._motion_group_configure_new)
         self._run_widget.remove_mg_btn.clicked.connect(self._motion_group_remove_from_rm)
         self._run_widget.modify_mg_btn.clicked.connect(self._motion_group_modify_existing)
 
