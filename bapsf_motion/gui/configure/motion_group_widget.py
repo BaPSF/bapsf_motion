d60fef6885ddfdf13c15adda2fe68fc4182f68b1
Erik <eteverson@gmail.com>
2026-08-05 13:52:17 -0700

    ConfigureGUI : remove property rm (access rm through rmo)

--- bapsf_motion/gui/configure/motion_group_widget.py
+++ bapsf_motion/gui/configure/motion_group_widget.py
@@ -418,8 +418,8 @@ class MGWidget(QWidget):
         # gather deployed restricted values
         deployed_mg_names = []
         deployed_ips = []
-        if isinstance(self._parent.rm, RunManager):
-            for mg in self._parent.rm.mgs.values():
+        if isinstance(self._parent.rmo.rm, RunManager):
+            for mg in self._parent.rmo.rm.mgs.values():
                 if (
                     mg_config is not None
                     and mg_config["name"] == mg.config["name"]
