a034ca9c5edacb85ecbda8b629cac573cf9d12b0
Erik <eteverson@gmail.com>
2026-08-26 15:25:17 -0700

    add left and right arrows to icon_name_dict

--- bapsf_motion/gui/icons.py
+++ bapsf_motion/gui/icons.py
@@ -6,3 +6,10 @@ widgets.
 __all__ = ["icon_name_dict"]
 
 from bapsf_qt.utils import icon_name_dict
+
+# add left and right arrows ... this needs to be piped to bapsf_qt
+family = icon_name_dict["arrow-down"].split(".")[0]
+icon_name_dict.update({
+    "arrow-left": f"{family}.arrow-left",
+    "arrow-right": f"{family}.arrow-right",
+})
