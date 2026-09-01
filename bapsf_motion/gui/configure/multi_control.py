66dcc4af8c8b3cb0372b369920f2e5b9db90f2bb
Erik <eteverson@gmail.com>
2026-08-31 18:04:09 -0700

    MGControlAxis : create method _actor_slot_movement_finished to translate the associated SimpleSignal to the movementStopped signal

--- bapsf_motion/gui/configure/multi_control.py
+++ bapsf_motion/gui/configure/multi_control.py
@@ -379,6 +379,9 @@ class MGControlAxis(QWidget):
         unit = position.unit
         return val * unit
 
+    def _actor_slot_movement_finished(self):
+        self.movementStopped.emit(self._ax_id)
+
     def motor_signals_connect(self):
         axis = self.axis
         if not isinstance(axis, Axis):
