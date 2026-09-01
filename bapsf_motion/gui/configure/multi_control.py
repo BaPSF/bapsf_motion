bf744e690dc01589631a198c2bc26bb46ac04522
Erik <eteverson@gmail.com>
2026-09-01 12:58:08 -0700

    MGControlAxis : create slot _handle_connection_established() and connect to establishedConnection signal

--- bapsf_motion/gui/configure/multi_control.py
+++ bapsf_motion/gui/configure/multi_control.py
@@ -129,6 +129,7 @@ class MGControlAxis(QWidget):
 
         self._actorStatusChanged.connect(self._handle_actor_status_changed)
         self.movementStopped.connect(self._handle_movement_stopped)
+        self.establishedConnection.connect(self._handle_connection_established)
 
         self.refreshDisplay.connect(self.update_displays)
 
@@ -617,6 +618,18 @@ class MGControlAxis(QWidget):
 
     @Slot()
     def _handle_actor_status_changed(self):
+        self.update_displays()
+        self.axisStatusChanged.emit()
+
+    @Slot()
+    def _handle_connection_established(self):
+        # Note: This slot needs to be trigger from a PySide6 signal and
+        #       not from any of the SimpleSignals attached to Motor.
+        #       Having the SimpleSignal execute this code risks the
+        #       execution of an unsafe thread operation.  The Motor
+        #       event-loop is executing in a different thread that is
+        #       unmanaged by PySide6.
+        self.setEnabled(True)
         self.update_displays()
         self.axisStatusChanged.emit()
 
