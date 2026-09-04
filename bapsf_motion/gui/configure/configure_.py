5b206e01db36fa319255b26106734639c194530b
Erik <eteverson@gmail.com>
2026-09-04 16:21:09 -0700

    ConfigureGUI : refactor _motion_group_multi_control -> _switch_to_run_control

--- bapsf_motion/gui/configure/configure_.py
+++ bapsf_motion/gui/configure/configure_.py
@@ -780,7 +780,7 @@ class ConfigureGUI(QMainWindow):
         self.run_widget.quit_btn.clicked.connect(self.discard_close)
         self.run_widget.mg_add_btn.clicked.connect(self._motion_group_configure_new)
         self.run_widget.mg_config_btn.clicked.connect(self._motion_group_configure_modify)
-        self.run_widget.mg_control_btn.clicked.connect(self._motion_group_multi_control)
+        self.run_widget.mg_control_btn.clicked.connect(self._switch_to_run_control)
 
     def _connect_signals_mg_widget(self):
         # Note: used during _spawn_mg_widget()
@@ -928,7 +928,7 @@ class ConfigureGUI(QMainWindow):
         self._switch_stack(which="configure")
 
     @Slot()
-    def _motion_group_multi_control(self):
+    def _switch_to_run_control(self):
         self._spawn_control_widget()
         self._switch_stack(which="control")
 
