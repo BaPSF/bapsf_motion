585a9406bb5101ff6c3198c1de8c5349f1a8d548
Erik <eteverson@gmail.com>
2026-09-09 10:45:42 -0700

    RunWidget : create attr update_display_timer (for updating the motion group list at a regular interval)

--- bapsf_motion/gui/configure/configure_.py
+++ bapsf_motion/gui/configure/configure_.py
@@ -15,7 +15,7 @@ import re
 
 from functools import partial
 from pathlib import Path
-from PySide6.QtCore import QDir, QObject, Qt, Signal, Slot
+from PySide6.QtCore import QDir, QObject, Qt, Signal, Slot, QTimer
 from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import (
     QApplication,
@@ -410,6 +410,7 @@ class RunWidget(QWidget):
         self.run_name_label = self._init_run_name_label()
         self.run_name_widget = self._init_run_name_widget()
         self.toml_widget = self._init_toml_widget()
+        self.update_display_timer = self._init_update_display_timer()
 
         self.setLayout(self._define_layout())
         self._connect_signals()
@@ -583,6 +584,12 @@ class RunWidget(QWidget):
         )
         _widget.setFixedWidth(500)
         return _widget
+
+    def _init_update_display_timer(self):
+        timer = QTimer(parent=self)
+        timer.setInterval(500)
+        timer.setSingleShot(True)
+        return timer
 
     @Slot()
     def enable_mg_buttons(self):
