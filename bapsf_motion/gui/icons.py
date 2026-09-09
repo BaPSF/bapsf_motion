ce309b4b4cdc1969a4903e4b82bac0927bfd4fe4
Erik <eteverson@gmail.com>
2026-09-09 16:34:15 -0700

    add to icon_name_dict "park" and "park-solid"

--- bapsf_motion/gui/icons.py
+++ bapsf_motion/gui/icons.py
@@ -13,6 +13,8 @@ icon_name_dict.update(
     {
         "arrow-left": f"{family}.arrow-left",
         "arrow-right": f"{family}.arrow-right",
+        "park": "ri.parking-box-line",
+        "park-solid": "ri.parking-box-fill",
         "robot-dead": "mdi.robot-dead-outline",
         "wifi-offline": "ri.wifi-off-line",
         "wifi-online": "ri.wifi-line",
