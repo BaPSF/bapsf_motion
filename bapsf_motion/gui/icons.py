bc7dcaf6da7e08bb2c2e9b55daf712fc681a3aab
Erik <eteverson@gmail.com>
2026-09-09 10:40:41 -0700

    add icons "robot-dead", "wifi-offline", and "wifi-online" to icon_name_dict

--- bapsf_motion/gui/icons.py
+++ bapsf_motion/gui/icons.py
@@ -9,7 +9,12 @@ from bapsf_qt.utils import icon_name_dict
 
 # add left and right arrows ... this needs to be piped to bapsf_qt
 family = icon_name_dict["arrow-down"].split(".")[0]
-icon_name_dict.update({
-    "arrow-left": f"{family}.arrow-left",
-    "arrow-right": f"{family}.arrow-right",
-})
+icon_name_dict.update(
+    {
+        "arrow-left": f"{family}.arrow-left",
+        "arrow-right": f"{family}.arrow-right",
+        "robot-dead": "mdi.robot-dead-outline",
+        "wifi-offline": "ri.wifi-off-line",
+        "wifi-online": "ri.wifi-line",
+    }
+)
