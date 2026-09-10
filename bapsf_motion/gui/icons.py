b5a4343fa42780d702fa80a0148f2c2208cb9ea2
Erik Everson <eteverson@gmail.com>
2026-09-10 09:41:03 -0700

    Add to `icon_name_dict` (#224)
    * add left and right arrows to icon_name_dict
    
    * add icons "robot-dead", "wifi-offline", and "wifi-online" to icon_name_dict
    
    * add to icon_name_dict "park" and "park-solid"

--- bapsf_motion/gui/icons.py
+++ bapsf_motion/gui/icons.py
@@ -6,3 +6,17 @@ widgets.
 __all__ = ["icon_name_dict"]
 
 from bapsf_qt.utils import icon_name_dict
+
+# add left and right arrows ... this needs to be piped to bapsf_qt
+family = icon_name_dict["arrow-down"].split(".")[0]
+icon_name_dict.update(
+    {
+        "arrow-left": f"{family}.arrow-left",
+        "arrow-right": f"{family}.arrow-right",
+        "park": "ri.parking-box-line",
+        "park-solid": "ri.parking-box-fill",
+        "robot-dead": "mdi.robot-dead-outline",
+        "wifi-offline": "ri.wifi-off-line",
+        "wifi-online": "ri.wifi-line",
+    }
+)
