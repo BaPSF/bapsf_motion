bd308d44ba4cb0c83644ca508561c3f33be6ddec
Erik <eteverson@gmail.com>
2026-09-10 11:10:25 -0700

    RunWidget.update_display_mg_list : add kwarg "rewrite" (default True)

--- bapsf_motion/gui/configure/configure_.py
+++ bapsf_motion/gui/configure/configure_.py
@@ -679,10 +679,14 @@ class RunWidget(QWidget):
         rm_name = rm.config["name"]
         self.run_name_widget.setText(rm_name)
 
-    def update_display_mg_list(self):
-        self.mg_list_widget.clear()
-        self.mg_remove_btn.setEnabled(False)
-        self.mg_config_btn.setEnabled(False)
+    def update_display_mg_list(self, rewrite: bool = True):
+        # rewrite = True then the whole list will be rewritten
+        # rewrite = False contents are just refreshed
+        #
+        if rewrite:
+            self.mg_list_widget.clear()
+            self.mg_remove_btn.setEnabled(False)
+            self.mg_config_btn.setEnabled(False)
 
         rm = self.rm
         if not isinstance(rm, RunManager) or len(rm.mgs) == 0:
@@ -693,8 +697,8 @@ class RunWidget(QWidget):
             self.logger.info(f"Adding to MG List - {label}")
 
             is_valid = True
-            tooltip = None
             if not mg.connected:
+            tooltip = ""
                 is_valid = False
                 tooltip = "TCP connection not successful for all axes."
             elif not isinstance(mg.mb, MotionBuilder):
@@ -714,12 +718,26 @@ class RunWidget(QWidget):
                 else qta.icon(icon_name_dict["check-circle"], color="green")
             )  # type: QIcon
 
-            _item = QListWidgetItem(
-                _icon,
-                label,
-                listview=self.mg_list_widget,
-            )
-            if not is_valid and tooltip is not None:
+            if rewrite:
+                _item = QListWidgetItem(
+                    _icon,
+                    label,
+                    listview=self.mg_list_widget,
+                )
+                _item.setToolTip(tooltip)
+            else:
+                items = self.mg_list_widget.findItems(label, Qt.MatchFlag.MatchExactly)
+
+                if len(items) != 1:
+                    # The motion groups defined in the RunManager do NOT perfectly
+                    # match the current list widget contents.  Must do a rewrite
+                    # instead of just a refresh.
+                    #
+                    self.updateDisplays.emit()
+                    return
+
+                _item = items[0]
+                _item.setIcon(_icon)
                 _item.setToolTip(tooltip)
 
     def closeEvent(self, event: QCloseEvent):
